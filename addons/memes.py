96cde3d983bd3db6fa7fb5b7d850dabd2b1dcf29
sixardyH <cyril.devaucleroy@gmail.com>
2017-08-15 23:25:09 +0200

    Fix EdTheNerd's code again

--- addons/memes.py
+++ addons/memes.py
@@ -28,13 +28,13 @@ class Memes:
         """Goddamn Nazimod"""
         return await self.bot.say("https://i.imgur.com/4kANai8.png")
 
-    @commands.command
-    async def bigsmoke(self, ctx):
+    @commands.command()
+    async def bigsmoke(self):
         """Memes."""
         await self.bot.say(ctx, "http://i.imgur.com/vo5l6Fo.jpg\nALL YOU HAD TO DO WAS FOLLOW THE DAMN GUIDE CJ!")
  
-    @commands.command
-    async def bigorder(self, ctx):
+    @commands.command()
+    async def bigorder(self):
         """Memes."""
         await self.bot.say(ctx, "I'll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda.")
  
