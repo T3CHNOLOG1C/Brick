f3591ec0d353f86f25a6d7416e1c5048ed98a380
EdTheNerd <31045942+EdTheNerd@users.noreply.github.com>
2017-08-15 16:49:53 -0400

    Update memes.py

--- addons/memes.py
+++ addons/memes.py
@@ -28,15 +28,15 @@ class Memes:
         """Goddamn Nazimod"""
         return await self.bot.say("https://i.imgur.com/4kANai8.png")
 
-    @commands.command(pass_context=True, hidden=True)
+    @commands.command
      async def bigsmoke(self, ctx):
         """Memes."""
-        await self._meme(ctx, "http://i.imgur.com/vo5l6Fo.jpg\nALL YOU HAD TO DO WAS FOLLOW THE DAMN GUIDE CJ!")
+        await self.bot.say(ctx, "http://i.imgur.com/vo5l6Fo.jpg\nALL YOU HAD TO DO WAS FOLLOW THE DAMN GUIDE CJ!")
  
-    @commands.command(pass_context=True, hidden=True)
+    @commands.command
      async def bigorder(self, ctx):
         """Memes."""
-        await self._meme(ctx, "I’ll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda.")
+        await self.bot.say(ctx, "I’ll have two number 9s, a number 9 large, a number 6 with extra dip, a number 7, two number 45s, one with cheese, and a large soda.")
  
 def setup(bot):
     bot.add_cog(Memes(bot))
