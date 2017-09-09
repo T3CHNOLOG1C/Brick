70d789800068d6c0f394665c94f5f8aed5bb8e3e
Laine <T3CHNOLOG1C@users.noreply.github.com>
2017-09-08 22:46:28 -0400

    ugh python is hard

--- addons/misc.py
+++ addons/misc.py
@@ -28,7 +28,7 @@ class Misc:
     @commands.command()
     async def membercount(self):
         """Prints current member count"""
-        return await self.bot.say("This server's current member count is:" + len(guild.members))
+        return await self.bot.say("This server's current member count is:" + len(Discord.guild.members))
     
     @commands.command()
     async def about(self):
