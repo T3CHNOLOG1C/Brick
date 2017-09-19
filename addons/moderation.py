46d03278813c2bd06f573d10142c09837474af9e
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-09-18 18:26:50 -0700

    Wtf

--- addons/moderation.py
+++ addons/moderation.py
@@ -81,35 +81,5 @@ class Moderation:
             except discord.errors.Forbidden:
                 await self.bot.say("💢 Couldn't ban {}".format(member))
 
-    @commands.command(pass_context=True, hidden=True, name="pull", aliases=["pacman"])
-    async def pull(self, ctx, pip=None):
-        """Pull new changes from Git and restart.
-        Append -p or --pip to this command to also update python modules from requirements.txt.
-        """
-        dev = ctx.message.author
-        if self.bot.botdev_role in dev.roles or self.bot.owner_role in dev.roles:
-            await self.bot.say("`Pulling changes...`")
-            call(["git", "pull"])
-            pip_text = ""
-            if pip == "-p" or pip == "--pip" or pip == "-Syu":
-                await self.bot.say("`Updating python dependencies...`")
-                call(["python3.6", "-m", "pip", "install", "--user", "-r",
-                    "requirements.txt"])
-                pip_text = " and updated python dependencies"
-            await self.bot.say("Pulled changes{}! Restarting...".format(pip_text))
-            execv("./Brick.py", argv)
-        else:
-            if "pacman" in ctx.message.content:
-                await self.bot.say("`{} is not in the sudoers file. This incident will be reported.`".format(ctx.message.author.display_name))
-            else:
-                await self.bot.say("Only bot devs and / or owners can use this command")
-
-    @commands.has_permissions(administrator=True)
-    @commands.command()
-    async def restart(self):
-        """Restart the bot (Staff Only)"""
-        await self.bot.say("`Restarting, please wait...`")
-        execv("./Brick.py", argv)
-
 def setup(bot):
     bot.add_cog(Moderation(bot))
