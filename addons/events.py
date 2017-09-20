2104c29b2f77e89e8580a3b34df873c8093eb4d7
Laine <T3CHNOLOG1C@users.noreply.github.com>
2017-09-20 11:21:30 -0400

    add fefosheep to releases

--- addons/events.py
+++ addons/events.py
@@ -39,6 +39,7 @@ class Events:
             "astronautlevel2": "Anemone3DS",
             "T3CHNOLOG1C": "Discord-ChromeOS",
             "T3CHNOLOG1C": "DiscordCanary-ChromeOS",
+            "kekmaster97": "fefosheep",
         }
         bot.loop.create_task(self.new_releases())
         print("{} addon loaded.".format(self.__class__.__name__))
