5145a84673cfaaf13db40132954c0fb4749dc176
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-10-13 21:24:26 -0700

    Add find_user helper func to speak.py

--- addons/speak.py
+++ addons/speak.py
@@ -9,6 +9,20 @@ class Speak:
     def __init__(self, bot):
         self.bot = bot
         print("{} addon loaded.".format(self.__class__.__name__))
+        
+    def find_user(self, user, ctx):
+        found_member = self.bot.guild.get_member(user)
+        if not found_member:
+            found_member = self.bot.guild.get_member_named(user)
+        if not found_member:
+            try:
+                found_member = ctx.message.mentions[0]
+            except IndexError:
+                pass
+        if not found_member:
+            return None
+        else:
+            return found_member
 
     @commands.has_permissions(manage_messages=True)
     @commands.command(pass_context=True)
@@ -19,7 +33,7 @@ class Speak:
             channel = ctx.message.channel_mentions[0]
             await channel.send(message)
 
-    async def memberDM(self, ctx, member, message):
+    async def memberDM(self, ctx, found_member, message):
         try:
             if ctx.message.attachments:
                 attachment_urls = []
@@ -29,20 +43,20 @@ class Speak:
             await ctx.send("Complete!")
             return message
             if len(message) > 2000:
-                await member.send(message[:2000])
-                await member.send(message[2000:])
+                await found_member.send(message[:2000])
+                await found_member.send(message[2000:])
             else:
-                await member.send(message)
+                await found_member.send(message)
         except discord.errors.Forbidden:
-            await self.bot.logs_channel.send("Couldn't send message to {}.".format(member.mention))
+            await self.bot.logs_channel.send("Couldn't send message to {}.".format(found_member.mention))
 
     # @commands.has_permissions(administrator=True)
     @commands.command(pass_context=True)
     async def dm(self, ctx, member, *, message):
         """DM a user. (Staff Only)"""
         await ctx.message.delete()
-        member = ctx.message.mentions[0]
-        await self.memberDM(ctx, member, message)
+        found_member = self.find_user(member, ctx)
+        await self.memberDM(ctx, found_member, message)
 
     @commands.has_permissions(administrator=True)
     @commands.command(pass_context=True)
@@ -50,8 +64,9 @@ class Speak:
         """DM multiple users. (Staff Only)
         Message has to be between quotes, and before the mentions."""
         await ctx.message.delete()
-        for member in ctx.message.mentions:
-            await self.memberDM(member, message)
+        for members in ctx.message.mentions:
+            found_member = self.find_user(member, ctx)
+            await self.memberDM(found_member, message)
     
     @commands.has_permissions(administrator=True)
     @commands.command(pass_context=True)
@@ -61,13 +76,13 @@ class Speak:
         async for m in self.bot.brickdms_channel.history(limit=250):
                 try:
                     if ctx.message.author == self.bot.user:
-                        member = m.mentions[0]
+                        found_member = m.mentions[0]
                         break
                     else:
                         continue
                 except IndexError:
                     continue
-        await self.memberDM(member, message)
+        await self.memberDM(found_member, message)
 
 def setup(bot):
     bot.add_cog(Speak(bot))
